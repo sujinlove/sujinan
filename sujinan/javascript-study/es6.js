8022c1b07c6b82734a40e48f9211fcea9342933b
sujinlove <soojin9491@mail.com>
2021-02-22 06:31:28 +0900

    ES6 Study - Spread Syntax

--- sujinan/javascript-study/es6.js
+++ sujinan/javascript-study/es6.js
@@ -68,3 +68,40 @@
 
   // desctructuring assignment에서 object를 사용할 때는 {}, 배열은 [] 사용
 }
+
+/* Spread Syntax */
+
+{
+  const obj1 = { key: "key1" };
+  const obj2 = { key: "key2" };
+  const array = [obj1, obj2];
+
+  // Array Copy
+  const arrayCopy = [...array];
+
+  // ... => array안에 들어있는 오브젝트를 각각 한개씩 가져와서 복사
+  // => 오브젝트의 주소 값을 가져와서 복사!!
+
+  console.log(array, arrayCopy);
+
+  // 배열 복사 후 배열에 새로운 오브젝트를 추가하고 싶다면
+  const arrayCopy2 = [...array, { key3: "key3" }];
+  console.log(arrayCopy2);
+
+  // Object
+  const obj3 = { ...obj1 };
+  console.log(obj3);
+
+  // array concatenation
+  const fruits1 = ["apple", "banana", "kiwi"];
+  const fruits2 = ["grape", "mango", "pineapple"];
+  const fruits = [...fruits1, ...fruits2];
+  console.log(fruits);
+
+  // object merge
+  const dog1 = { dog: "말티즈" };
+  const dog2 = { dog: "시츄" };
+  const dog = { ...dog1, ...dog2 };
+  console.log(dog);
+  // Object는 Key가 같으면 마지막으로 복사한 오브젝트의 키값이 덮여쓰여진다.
+}
